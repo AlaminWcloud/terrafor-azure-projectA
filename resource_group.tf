c45d871764c802d922d0ff9060704bb1700db5fa
ms_wanka_cloud <darkvader26@icloud.com>
2026-08-02 22:50:47 +0300

    adding all changes

---
+++ resource_group.tf
@@ -0,0 +1,9 @@
+resource "azurerm_resource_group" "rg-projectA-dev-uks-001" {
+  name     = var.rescource_group_name
+  location = var.location
+
+  tags = {
+    "name"       = "projectA"
+    "enviroment" = "dev"
+  }
+}
---
+++ variabls.tf
@@ -0,0 +1,27 @@
+variable "rescource_group_name" {
+  description = "the name of the resource group"
+  type        = string
+  default     = "rg-projectA-dev-uks-001"
+
+}
+variable "virtual_network_name" {
+  description = "virtual network name"
+  type        = string
+  default = "vnet-projectA-dev-uks-001"
+
+}
+
+variable "address_prefixes" {
+  type    = list(string)
+  default = ["10.0.0.0/24", "10.0.1.0/24"]
+}
+variable "address_space" {
+    type = set(string)
+    default = [ "10.0.0.0/16" ]
+  
+}
+variable "location" {
+    type = string
+    default = "uk south"
+  
+}
