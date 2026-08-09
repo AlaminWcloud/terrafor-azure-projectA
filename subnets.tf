fa47f8911e365ca587bc3e25fef72eececc4b45a
ms_wanka_cloud <darkvader26@icloud.com>
2026-08-09 21:55:25 +0300

    refenrcing

--- subnets.tf
+++ subnets.tf
@@ -2,5 +2,5 @@ resource "azurerm_subnet" "Vm-subnet" {
   name                 = var.linux_vm_subnet_name
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.vnet-projectA-dev-uks-001.name
-  address_prefixes     = [var.address_space[0]]
+  address_prefixes     = [var.address_prefixes[0]]
 }
